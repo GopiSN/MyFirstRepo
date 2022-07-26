19aff0deee38cb1ad1d5631a50ec1dd9a464f582
Gopilakshmi S N <gopilakshmi.sn@elektrobit.com>
2022-07-26 13:45:39 +0530

    Adding the Ceedling project for Unit Testing

---
+++ testProject/build/test/dependencies/test_bit_manipulation.d
@@ -0,0 +1,4 @@
+build/test/out/c/test_bit_manipulation.o: test/test_bit_manipulation.c \
+ C:/Ruby30-x64/lib/ruby/gems/3.0.0/gems/ceedling-0.31.1/vendor/unity/src/unity.h \
+ C:/Ruby30-x64/lib/ruby/gems/3.0.0/gems/ceedling-0.31.1/vendor/unity/src/unity_internals.h \
+ src/bit_manipulation.h
